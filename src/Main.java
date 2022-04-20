e36a4032dbb00d276b3518e661e0526c3afe8cb7
Agnieszka Bunia <agabuuuunia@gmail.com>
2022-04-20 17:50:34 +0200

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String month = "April";
-        int year = 2022;
-        long day = 19;
-        System.out.println("miesiąc:" + month );
-        System.out.println("rok:" + year );
-        System.out.println("dzień:" + day );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Jak masz na imię?");
+        
+        String name = scanner.nextLine();
+        System.out.println("Cześć " + name);
+
+        System.out.println("Jak masz na nazwisko?");
+        String lastname = scanner.nextLine();
+
+        System.out.println("Ile masz lat?");
+        
+        int age = scanner.nextInt();
+
+        if (age > 18){
+            System.out.println("jest super!");
+        } else {
+            System.out.println("jest lipa!");
+        }
     }
 }
